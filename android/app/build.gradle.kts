2cf7c858a229d1a0bb7556c389e746d2f8d49b4a
hamrichamine699-creator <hamrichamine699@gmail.com>
2026-06-17 04:16:22 +0100

    Add build.gradle.kts for Android app configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.warsh_quran_app"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.warshquran"
+        minSdk = 21
+        targetSdk = 34
+    }
+}
